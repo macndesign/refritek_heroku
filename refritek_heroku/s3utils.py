8f7a3ec55ce5b345ed3864e0ed17be53f58116a6
macndesign <macndesign@gmail.com>
2013-06-25 16:25:53 -0300

    tentativa 4 de conserto do s3 com estaticos

--- refritek_heroku/s3utils.py
+++ refritek_heroku/s3utils.py
@@ -4,14 +4,9 @@ from django.conf import settings
 
 class S3StaticStorage(S3BotoStorage):
     def url(self, name):
-        """
-        Change URL's generated in the following format: http(s)://bucket.name.s3.amazonaws.com/.../
-        Into the format: http(s)://s3.amazonaws.com/bucket.name/.../
-        """
         url = super(S3StaticStorage, self).url(name)
-        bucket_name = self.bucket.name
-        if "%s.s3.amazonaws.com" % bucket_name in url:
-            url = url.replace("%s.s3.amazonaws.com/" % bucket_name, "s3.amazonaws.com/%s/" % bucket_name)
+        if name.endswith('/') and not url.endswith('/'):
+            url += '/'
         return url
 
 
