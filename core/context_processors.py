ee5bf604cbbda36d79a1753a100de61df7e5fe7a
macndesign <macndesign@gmail.com>
2013-06-25 22:16:22 -0300

    conserto em ctx proc 1

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,6 +1,12 @@
 from .models import Pagina
+from django.shortcuts import get_object_or_404
 
 
 def pagina_context_processors(request):
-    pagina = Pagina.objects.latest('data_criacao')
+    pagina = None
+
+    if Pagina.objects.latest('data_criacao'):
+        pagina = Pagina.objects.latest('data_criacao')
+        pagina = get_object_or_404(pagina)
+
     return {'pagina': pagina}
