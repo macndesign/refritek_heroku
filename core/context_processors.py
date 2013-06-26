839c1381cac8927c6c7f4304c4747522493f024b
macndesign <macndesign@gmail.com>
2013-06-25 23:53:01 -0300

    conserto em ctx proc 4

--- core/context_processors.py
+++ core/context_processors.py
@@ -3,5 +3,5 @@ from django.shortcuts import get_object_or_404
 
 
 def pagina_context_processors(request):
-    pagina = get_object_or_404(Pagina)
+    pagina = get_object_or_404(Pagina, pk=1)
     return {'pagina': pagina}
--- refritek_heroku/settings.py
+++ refritek_heroku/settings.py
@@ -6,7 +6,7 @@ import dj_database_url
 
 PROJECT_DIR = Path(__file__).parent
 
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
