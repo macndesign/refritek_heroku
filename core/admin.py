ac05b6bc1355755fc49d4db108ee5a89df57ae81
macndesign <macndesign@gmail.com>
2013-07-24 11:46:47 -0300

    adicionado pra aceitar cartoes em todas as paginas e melhoria na usabilidade do admin de produtos para buscar por nome do produto e empresa e mostrar alguns campos na listagem

--- core/admin.py
+++ core/admin.py
@@ -3,9 +3,15 @@ from django.contrib import admin
 from .models import Empresa, Pagina, Produto, Slider, Sobre, Contato, Cartao
 from south.models import MigrationHistory
 
+
+class ProdutoAdmin(admin.ModelAdmin):
+    search_fields = ('nome', 'empresa')
+    list_display = ('nome', 'empresa', 'destaque', 'ativo')
+
+admin.site.register(Produto, ProdutoAdmin)
+
 admin.site.register(Empresa)
 admin.site.register(Pagina)
-admin.site.register(Produto)
 admin.site.register(Slider)
 admin.site.register(Sobre)
 admin.site.register(Contato)
